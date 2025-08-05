bbb5c1eae66cf36cc829ff5c977e3a502cf94b29
looksmelt <looksmelt@outlook.com>
2025-08-05 20:33:16 +0800

    Create 转换时间戳.py

---
+++ 转换时间戳.py
@@ -0,0 +1,8 @@
+import time
+ 
+# 获得当前时间时间戳
+now = int(time.time())
+#转换为其他日期格式,如:"%Y-%m-%d %H:%M:%S"
+timeArray = time.localtime(now)
+otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+print(otherStyleTime)
